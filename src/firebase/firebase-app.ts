68a96d9990bac350175074576983168eb6206ab2
Sebastian Jørgensen <sj@invokit.dk>
2018-09-27 09:01:41 +0200

    removed non-firebase code

--- src/firebase/firebase-app.ts
+++
@@ -1,6 +0,0 @@
-import firebase from "firebase-admin";
-
-// Use default credentials
-const app = firebase.initializeApp();
-
-export default app;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import oauth2App from "./oauth2-app";
-import { MemoryTokenStorage } from "./token-storage";
-
-const app = oauth2App(new MemoryTokenStorage());
-app.listen(8080);
